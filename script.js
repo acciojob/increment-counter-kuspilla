24ab5cac775be0deced3a59863da787cbf814f71
Dasthu <118829965+kuspilla@users.noreply.github.com>
2023-11-22 08:20:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+const counterElement = document.getElementById('counter');
+    const incrementButton = document.getElementById('incrementBtn');
+
+    // Initialize the counter value
+    let counterValue = 0;
+
+    // Add a click event listener to the increment button
+    incrementButton.addEventListener('click', () => {
+      // Display an alert with the current un-incremented value
+      alert(`Count: ${counterValue}`);
+
+      // Increment the counter value
+      counterValue++;
+
+      // Update the counter element with the new value
+      counterElement.textContent = counterValue;
+
+	});
